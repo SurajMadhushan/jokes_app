eee8ea5de508a810e846152c89bc425d32728987
Suraj Madhushan <surajmshan1234@gmail.com>
2024-11-30 12:58:37 +0530

    modified

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:jokes_app/controllers/joke_controller.dart';
-
 import 'models/joke_model.dart';
 
 void main() {
@@ -15,16 +14,27 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   final JokeController _jokeController = JokeController();
   late Future<String> _jokes;
 
   @override
   void initState() {
     super.initState();
-    _jokes = _jokeController.getJokes();
+    _loadJokes();
   }
 
+  // Function to load jokes
+  void _loadJokes() {
+    setState(() {
+      _jokes = _jokeController.getJokes();
+    });
+  }
+
+  // Function to handle refresh action
+  Future<void> _onRefresh() async {
+    _loadJokes(); // Reload jokes
+    await Future.delayed(const Duration(seconds: 0)); // Optional: Simulate loading delay
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,45 +42,76 @@ class _MyAppState extends State<MyApp> {
       title: "Jokes App",
       home: Scaffold(
         appBar: AppBar(
-          title: Center(
-              child: Text(
-                  'Jokes App',
-                style: TextStyle(fontSize: 26,
-                color: Colors.white,
-                letterSpacing: 10),
-          )),
           backgroundColor: Colors.tealAccent,
+          elevation: 4,
+          centerTitle: true,
+          title: Text(
+            'Jokes App',
+            style: TextStyle(
+              fontSize: 26,
+              color: Colors.white,
+              letterSpacing: 10,
+            ),
+          ),
         ),
-        body: FutureBuilder<String>(
-          future: _jokes,
-          builder: (context, snapshot) {
-            if(snapshot.connectionState == ConnectionState.waiting) {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            else if(snapshot.hasError) {
-              return Center(
-                child: Text('Error occured: ${snapshot.error}'),
-              );
-            }
-            else if(snapshot.hasData){
-              final jokes = snapshot.data!;
-              return Text(
-                jokes
-              );
+        body: RefreshIndicator(
+          color: Colors.teal,
+          onRefresh: _onRefresh, // Trigger refresh when pulled down
+          child: FutureBuilder<String>(
+            future: _jokes,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.teal
+                  ),
+                );
+              } else if (snapshot.hasError) {
+                return Center(
+                  child: Text('Error occurred: ${snapshot.error}'),
+                );
+              } else if (snapshot.hasData) {
+                final jokes = snapshot.data!;
+                return SingleChildScrollView(
+                  physics: const AlwaysScrollableScrollPhysics(),
+                  child: Container(
+                    margin: const EdgeInsets.all(16.0),
 
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(8.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.4),
+                          spreadRadius: 5,
+                          blurRadius: 10,
+                          offset: const Offset(0, 3),
+                        ),
+                      ],
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        jokes,
+                        style: TextStyle(
+                          color: Colors.teal,
+                          fontSize: 18.0,
 
-            }
-            else{
-              return Center(
-                child: Text('No jokes available'),
-              );
-            }
-          },
-        )
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                );
+              } else {
+                return const Center(
+                  child: Text('No jokes available'),
+                );
+              }
+            },
+          ),
+        ),
       ),
     );
   }
 }
-
--- lib/services/joke_service.dart
+++ lib/services/joke_service.dart
@@ -8,19 +8,25 @@ class JokeService{
   Future<String> getJokes() async {
     try {
       final response = await _dio.get('https://v2.jokeapi.dev/joke/Any');
+
       if (response.statusCode == 200) {
         final data = response.data;
-        print(data);
+        print(data['type']);
+
         if (data['type'] == 'twopart') {
           return '${data['setup']} - ${data['delivery']}'; // Two-part joke
-        } else {
+        }
+        else if(data['type'] == 'single') {
+          return '${data['joke']}';
+        }
+        else {
           throw Exception('Unexpected joke format');
         }
       } else {
         throw Exception('Failed to fetch jokes: ${response.statusCode}');
       }
     } catch (error) {
-      throw Exception('Error occurred: $error');
+      throw Exception('Error occurred2: $error');
     }
   }
 }
